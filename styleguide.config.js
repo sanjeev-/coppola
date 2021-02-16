ab2629aecae0c160994515e18ecbcb4d9b8f4634
sanjeev- <sanjeev87@gmail.com>
2021-02-16 18:00:50 -0500

    add production build

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  components: 'src/components/**/[A-Z]*.tsx'
+  pagePerSection: true,
+  components: 'src/components/**/[A-Z]*.tsx',
+  usageMode: 'expand',
+  title: 'Coppola React Component Library',
+  sections: [
+    {
+      name: 'Typography',
+      components: [
+        'src/components/Text/[A-Z]*.tsx',
+        'src/components/Link/[A-Z]*.tsx'
+      ],
+      sectionDepth: 1
+    },
+    {
+      name: 'UI',
+      components: ['src/components/Flex/[A-Z]*.tsx'],
+      sectionDepth: 1
+    }
+  ]
 }
---
+++ styleguide/precache-manifest.48b467fbbc8db24facde58be926443b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2645e282f3fe1f7545bc",
+    "url": "build/2.2645e282.js"
+  },
+  {
+    "revision": "ec6d6602d3ea186a8f5f4a704228519a",
+    "url": "build/2.2645e282.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7dce726b4bc5d5e8992",
+    "url": "build/bundle.a7dce726.js"
+  },
+  {
+    "revision": "328eec8288905787cd0c",
+    "url": "build/main.328eec82.js"
+  },
+  {
+    "revision": "1462f0689c7853a463280aef54a23bb6",
+    "url": "index.html"
+  },
+  {
+    "revision": "328eec8288905787cd0c",
+    "url": "static/css/main.6bed6731.chunk.css"
+  },
+  {
+    "revision": "fd0ed435ec08aae4c87867bf3068762d",
+    "url": "static/media/JosefinSans-Italic.fd0ed435.ttf"
+  },
+  {
+    "revision": "324a513cb9c683dc30777bdbccf59995",
+    "url": "static/media/JosefinSans-Regular.324a513c.ttf"
+  }
+]);
